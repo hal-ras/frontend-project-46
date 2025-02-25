555febf3189301c769cc6cd8a8c2cc21375392b0
Hal Ras <rasulhalilov@gmail.com>
2025-02-25 22:50:16 +0300

    add gendiff.js, index.js, parser.js

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import app from '../src/index.js'
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
+    .action((filepath1, filepath2, options) => {
+        console.log(app(filepath1, filepath2, options));
+    })
+    .helpOption('-h, --help', 'output usage information');
+
+program.parse();
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import path from 'path';
+import fs from 'fs';
+import parser from './parser.js';
+
+export default (filepath1, filepath2) => {
+    const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
+    const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
+    const parsedData1 = parser(content1);
+    const parsedData2 = parser(content2);
+    console.log(parsedData1);
+    console.log(parsedData2);
+    return content1 === content2;
+};
