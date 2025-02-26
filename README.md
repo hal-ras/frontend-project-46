0198699f248d7fb1e9faaef1e5ef8bc7af050c1f
Hal Ras <rasulhalilov@gmail.com>
2025-02-26 23:25:52 +0300

    update index.js, add gendiff json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/hal-ras/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/hal-ras/frontend-project-46/actions)
+
+### Gendiff JSON
+
+[![asciicast](https://asciinema.org/a/wq01LvEZQNUfRKTwLIjDPNjuB.svg)](https://asciinema.org/a/wq01LvEZQNUfRKTwLIjDPNjuB)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import path from 'path';
 import fs from 'fs';
+import _ from 'lodash';
 import parser from './parser.js';
 
 export default (filepath1, filepath2) => {
-    const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
-    const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
-    const parsedData1 = parser(content1);
-    const parsedData2 = parser(content2);
-    console.log(parsedData1);
-    console.log(parsedData2);
-    return content1 === content2;
+  const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
+  const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
+  const parsedData1 = parser(content1);
+  const parsedData2 = parser(content2);
+
+  const genDiff = (obj1, obj2) => {
+    const keys = _.sortBy([...Object.keys(obj1)], [...Object.keys(obj2)]);
+
+    const diff = keys.map((key) => {
+      if (!Object.hasOwn(obj2, key)) {
+        return `  - ${key}: ${obj1[key]}`;
+      }
+      if (!Object.hasOwn(obj1, key)) {
+        return `  + ${key}: ${obj2[key]}`;
+      }
+      if (obj1[key] !== obj2[key]) {
+        return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+      }
+      return `  ${key}: ${obj1[key]}`;
+    });
+    return `{\n${diff.join('\n')}\n}`;
+  };
+  return genDiff(parsedData1, parsedData2);
 };
