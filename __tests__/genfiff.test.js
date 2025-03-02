a3cbb40aa89d3e1734709045df051a0ecfb4b9fb
Hal Ras <rasulhalilov@gmail.com>
2025-03-02 14:07:12 +0300

    update index.js, add __tests__, workflow

---
+++ __tests__/genfiff.test.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiffJSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const expected = readFile('expectedJSON.txt').trim();
+  expect(gendiff(filepath1, filepath2)).toEqual(expected);
+});
--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import _ from 'lodash';
 import parser from './parser.js';
 
 export default (filepath1, filepath2) => {
-  const content1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
-  const content2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
+  const content1 = fs.readFileSync(
+    path.resolve(process.cwd(), '__fixtures__', filepath1),
+    'utf-8'
+  );
+  const content2 = fs.readFileSync(
+    path.resolve(process.cwd(), '__fixtures__', filepath2),
+    'utf-8'
+  );
   const parsedData1 = parser(content1);
   const parsedData2 = parser(content2);
 
   const genDiff = (obj1, obj2) => {
-    const keys = _.sortBy([...Object.keys(obj1)], [...Object.keys(obj2)]);
+    const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
     const diff = keys.map((key) => {
       if (!Object.hasOwn(obj2, key)) {
@@ -22,7 +28,7 @@ export default (filepath1, filepath2) => {
       if (obj1[key] !== obj2[key]) {
         return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
       }
-      return `  ${key}: ${obj1[key]}`;
+      return `    ${key}: ${obj1[key]}`;
     });
     return `{\n${diff.join('\n')}\n}`;
   };
